e24be929e3efccab9e607c9936b2bd53dc3dd316
Oscar Irribarra <osc-r@live.cl>
2020-04-20 22:33:59 -0400

    Se agrega Tabla Vehiculos

--- Migrations/20200421022707_AddVehicle.Designer.cs
+++ Migrations/20200421022707_AddVehicle.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VegaCore.Persistence;
 
 namespace VegaCore.Migrations
 {
     [DbContext(typeof(VegaCoreDbContext))]
-    partial class VegaCoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200421022707_AddVehicle")]
+    partial class AddVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Persistence/VegaCoreDbContext.cs
+++ Persistence/VegaCoreDbContext.cs
@@ -1,16 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using VegaCore.Models;
 
-namespace VegaCore.Persistence{
+namespace VegaCore.Persistence
+{
     public class VegaCoreDbContext : DbContext
     {
-        public VegaCoreDbContext(DbContextOptions<VegaCoreDbContext> options)
-            :base (options)
-        {
-            
-        }
-
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+
+        public VegaCoreDbContext(DbContextOptions<VegaCoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(x =>
+            new { x.VehicleId, x.FeatureId });
+        }
     }
 }
